19d3eb6263fe0975be529ed483414c9015046d83
herokim97 <ghkfud6545@gmail.com>
2023-02-14 02:15:12 +0900

    20230214

--- api/src/main/java/com/springboot/api/GetController/GetController.java
+++ api/src/main/java/com/springboot/api/GetController/GetController.java
@@ -1,6 +1,10 @@
 package com.springboot.api.GetController;
 
 import com.springboot.api.dto.MemberDto;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -9,25 +13,27 @@ import java.util.Map;
 @RequestMapping("/api/v1/get-api")
 
 public class GetController {
+    private final Logger LOGGER = LoggerFactory.getLogger(GetController.class);
     // http://localhost:8080/api/v1/get-api/variable1/{String 값}
     @GetMapping(value = "/variable1/{variable}")
     public String getVariable1(@PathVariable String variable)
     {
+        LOGGER.info("@PathVariable를 통해 들어온 값 : {}", variable);
         return variable;
     }
 
     //===================================================
 
     // http://loaclhost:8080/api/v1/get-api/request1?name=value&email=value2&organization=value3
-    @GetMapping(value = "/request1")
-    public String getRequestParam1(
-            @RequestParam String name,
-            @RequestParam String email,
-            @RequestParam String organization
-    )
-    {
-        return name + ", " + email + ", " + organization;
-    }
+//    @GetMapping(value = "/request1")
+//    public String getRequestParam1(
+//            @RequestParam String name,
+//            @RequestParam String email,
+//            @RequestParam String organization
+//    )
+//    {
+//        return name + ", " + email + ", " + organization;
+//    }
 
     //===================================================
 
@@ -52,6 +58,28 @@ public class GetController {
     public String getRequestParam3(MemberDto memberDto)
     {
         return memberDto.toString();
+    }
+
+    @ApiOperation(value = "GET 메서드 예제", notes="@RequestParam 활용한 GET Method")
+    @GetMapping(value = "/request1")
+    public String getRequestParam(
+            @ApiParam(value = "이름", required = true)@RequestParam String name,
+            @ApiParam(value = "이메일", required = true)@RequestParam String email,
+            @ApiParam(value = "회사", required = true)@RequestParam String org){
+        return name + " " + email + " " + org;
+    }
+
+   //http://localhost:8080/api/v1/get-api/hello
+    @RequestMapping(value="/hello", method = RequestMethod.GET)
+    public String getHello(){
+        LOGGER.info("getHello 메서드 호출");
+        return "Hello World";
+    }
+
+    @RequestMapping(value="/name", method = RequestMethod.GET)
+    public String getName(){
+        LOGGER.info("getName 메서드 호출");
+        return "Young Woong Kim";
     }
 
 
--- api/src/main/java/com/springboot/api/config/SwaggerConfiguration.java
+++ api/src/main/java/com/springboot/api/config/SwaggerConfiguration.java
@@ -1,7 +1,11 @@
 package com.springboot.api.config;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,12 +14,13 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+// 예제 5.22
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
 
     @Bean
-    public Docket api(){
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
@@ -24,11 +29,14 @@ public class SwaggerConfiguration {
                 .build();
     }
 
-    private ApiInfo apiInfo(){
+    private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Spring Boot Open API Test with Swagger")
-                .description("설명부분")
-                .version("1.0.0")
+                .description("Spring Boot Example")
+                .version("1.0.1")
                 .build();
     }
+
+
+
 }
